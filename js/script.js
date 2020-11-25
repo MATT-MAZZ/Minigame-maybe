528fd3b059eff00ef3ddc3eb01c18184ebda5d6c
MATT MAZZ <74719471+MATT-MAZZ@users.noreply.github.com>
2020-11-25 17:17:06 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ let counter = document.querySelector (".counter");
 let progress = document.querySelector (".progress");
 let goal = document.querySelector (".goal");
 let num = 0;
-let width = 30;
+let leftMove = -4005;
 
 
 
@@ -14,17 +14,16 @@ let width = 30;
 plus.addEventListener("click", () => {
     num++;
     counter.innerHTML = num;
-    width += 10;
-    goal.style.width = width + "px";
-    
+    leftMove += 10;
+    goal.style.left = leftMove + "px";
 });
 
 setInterval("minusWidth()", 250);
 
 function minusWidth () {
-    if (width > 30) {
-        width -= 10;
-        goal.style.width = width + "px"; 
+    if (leftMove > -4005) {
+        leftMove -= 10;
+        goal.style.left = leftMove + "px"; 
     }
 }
 

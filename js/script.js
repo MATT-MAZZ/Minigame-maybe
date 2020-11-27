0e41a66dc9c048fadf9f9d3ab7126a491e14a85a
MATT MAZZ <74719471+MATT-MAZZ@users.noreply.github.com>
2020-11-27 13:05:50 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,34 +1,167 @@
 let plus = document.querySelector (".plus");
-let minus = document.querySelector (".minus");
-let deleteC = document.querySelector (".delete");
 let counter = document.querySelector (".counter");
-
+let point = document.querySelectorAll ('.fas');
 let progress = document.querySelector (".progress");
 let goal = document.querySelector (".goal");
+let notice = document.querySelector(".notice");
 let num = 0;
 let width = 30;
-
+let windowWidth = +document.documentElement.clientWidth;
 
 
 
 plus.addEventListener("click", () => {
     num++;
     counter.innerHTML = num;
-    width += 10;
-    goal.style.width = width + "px";
     
+    switch (true) {
+        case (windowWidth > 1536): 
+        width += 10;
+        break;
+
+        case (windowWidth < 1440 && windowWidth > 1366): 
+        width += 8;
+        break;
+
+        case (windowWidth < 1366 && windowWidth > 1024): 
+        width += 7;
+        break;
+
+        case (windowWidth < 1024 && windowWidth > 480): 
+        width += 5;
+        break;
+    }
+    goal.style.width = width + "px";
+
+    buttonPress();
+
+    function buttonPress() {
+        plus.classList.add('plus_tap')
+        setTimeout(() => plus.classList.remove("plus_tap"), 100);
+    }
 });
 
-setInterval("minusWidth()", 250);
 
-function minusWidth () {
-    if (width > 30) {
-        width -= 10;
+
+setInterval(minusWidth, 250);
+
+function minusWidth() {
+
+        switch (true) {
+            case (width > 30 && windowWidth > 1536): 
+            width -= 10;
+            break;
+        
+            case (width > 30 && windowWidth < 1440 && windowWidth > 1366): 
+            width -= 8;
+            break;
+        
+            case (width > 30 && windowWidth < 1366 && windowWidth > 1024): 
+            width -= 7;
+            break;
+        
+            case (width > 30 && windowWidth < 1024 && windowWidth > 480): 
+            width -= 5;
+            break;
+        }
+        
         goal.style.width = width + "px"; 
     }
-}
+
+setInterval(pushNotice, 3000);
+
+    function pushNotice () {
+    
+        function negativeNotice() {
+            notice.classList.add("notice_negative");
+            notice.style.right = "0"
+            setTimeout(() => notice.classList.remove("notice_negative"), 2000);
+            setTimeout(() => notice.style.right = "", 2500);
+        }
+
+        function positiveNotice() {
+            notice.classList.add("notice_positive");
+            notice.style.left = "0"
+            setTimeout(() => notice.classList.remove("notice_positive"), 2000);
+            setTimeout(() => notice.style.left = "", 2500);
+        }
 
 
+        
+        let rNum2 = Math.floor(Math.random () * 101);
+        
+
+        switch (true) {
+            
+            case (rNum2 > 96 && rNum2 < 98 && width > 30):
+            positiveNotice();
+            notice.innerHTML = "<div>Победил<br>по жизни</div>";
+            setTimeout(() => notice.innerHTML = "", 2400);
+            width += 3000;
+            break;
+
+            case (rNum2 > 20 && rNum2 < 30 && width > 30):
+            positiveNotice();
+            notice.innerHTML = "<div>Вдохновение</div>";
+            setTimeout(() => notice.innerHTML = "", 2400);
+            width += 130;
+            break;
+
+            case (rNum2 > 30 && rNum2 < 45 && width > 30):
+            positiveNotice();
+            notice.innerHTML = "<div>Удача</div>";
+            setTimeout(() => notice.innerHTML = "", 2400);
+            width += 70;
+            break;
+
+            case (rNum2 > 1 && rNum2 < 20 && width > 30):
+            negativeNotice();
+            notice.innerHTML = "<div>Поленился</div>";
+            setTimeout(() => notice.innerHTML = "", 2400);
+            width -= 40;
+            break;
+
+            case (rNum2 > 45 && rNum2 < 50 && width > 30):
+            negativeNotice();
+            notice.innerHTML = "<div>Заболел</div>";
+            setTimeout(() => notice.innerHTML = "", 2400);
+            width -= 100;
+            break;
+
+            case (rNum2 > 50 && rNum2 < 60 && width > 30):
+            negativeNotice();
+            notice.innerHTML = "<div>Неудача</div>";
+            setTimeout(() => notice.innerHTML = "", 2400);
+            width -= 30;
+            break;
+
+            case (rNum2 > 98 && rNum2 < 100 && width > 30):
+            negativeNotice();
+            notice.innerHTML = "<div>Военкомат</div>";
+            setTimeout(() => notice.innerHTML = "", 2400);
+            width = 30;
+            break;
+
+            
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+// let bar_wraper = document.querySelectorAll('.bar_wraper')
+
+// point.forEach(function(elem) {
+//     elem.addEventListener("click", function () {
+//         this.classList.add("active");
+//     })
+// })
 
 
 
